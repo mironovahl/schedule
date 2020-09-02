626313c4f3b206373bacf0c20265af73b2e89bae
Евгений Лагутин <Runni007@yandex.ru>
2020-09-02 23:53:01 +0400

    fix: ts images

--- src/components/pages/main-page.tsx
+++ src/components/pages/main-page.tsx
@@ -7,7 +7,7 @@ const { Content } = Layout;
 
 const MainPage: React.FC = () => (
   <Layout style={{ minHeight: '100vh', background: '#fff' }}>
-    <Header />
+    <Header title="" />
     <Content style={{ margin: '16px 16px 32px' }}>
       <Link to="/schedule"><Button>Go to schedule</Button></Link>
     </Content>
