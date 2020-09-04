2768d5936f2e90dc40f5b1503b26584e0a9024ed
Oleg Rashinskiy <oleg.rashinskiy@gmail.com>
2020-09-04 20:15:41 +0300

    RS-19 add(husky): add husky with hooks rules

--- .prettierrc.js
+++ .prettierrc.js
@@ -2,6 +2,8 @@ module.exports = {
   trailingComma: 'all',
   tabWidth: 2,
   singleQuote: true,
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
   endOfLine: 'lf',
   printWidth: 100,
   arrowParens: 'always',
