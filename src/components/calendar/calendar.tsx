29ada4f29a84cfd4c5c19e3cd2335aa95e81b723
mergen <gaydaev.mergen@gmail.com>
2020-09-07 17:05:46 +0300

    feat: add responsive

--- src/components/calendar/calendar.tsx
+++ src/components/calendar/calendar.tsx
@@ -1,5 +1,12 @@
-import React from 'react';
-import { Calendar as AntDCalendar, Empty } from 'antd';
+import React, { useState } from 'react';
+import {
+  Calendar as AntDCalendar,
+  Empty,
+  Row,
+  Col,
+  Select,
+  Divider,
+} from 'antd';
 import * as moment from 'moment';
 import CalendarDate from './calendar-date';
 import CalendarMonth from './calendar-month';
@@ -44,13 +51,6 @@ const Calendar: React.FC<CalendarProps> = ({ dataSource }: CalendarProps) => {
   const getMonthData = (date: moment.Moment): IEvent[] => dataSource
     .filter((event) => moment(event.date).isSame(date, 'month'));
 
-  const dateCellRender = (date: moment.Moment): React.ReactNode => {
-    const data = eventsSortByDate(getDayData(date));
-    return data.length
-      ? <CalendarDate data={data} />
-      : null;
-  };
-
   const dateMonthRender = (date: moment.Moment): React.ReactNode => {
     const data = eventsSortByDate(getMonthData(date));
     return data.length
@@ -58,13 +58,85 @@ const Calendar: React.FC<CalendarProps> = ({ dataSource }: CalendarProps) => {
       : null;
   };
 
+  const [value, changeValue] = useState<moment.Moment>(moment(new Date()));
+  const [selectedValue, changeSelectedValue] = useState<moment.Moment>(moment(new Date()));
+
+  const onSelect = (value: moment.Moment): void => {
+    changeValue(value);
+    changeSelectedValue(value);
+  };
+
+  const onPanelChange = (value: moment.Moment): void => {
+    changeValue(value);
+  }
+
+  type gutterSettings = {
+    xs: number;
+    sm: number;
+    md: number;
+    lg: number;
+    xl: number;
+    xxl: number;
+  };
+
+  const rowGutterSettings: [gutterSettings, gutterSettings] = [
+    {
+      xs: 10,
+      sm: 12,
+      md: 14,
+      lg: 16,
+      xl: 18,
+      xxl: 20,
+    },
+    {
+      xs: 10,
+      sm: 12,
+      md: 14,
+      lg: 16,
+      xl: 18,
+      xxl: 20,
+    },
+  ];
+
   return (
     <div className="calendar">
-      <AntDCalendar
-        validRange={[getBeginDate(dates), getEndDate(dates)]}
-        dateCellRender={dateCellRender}
-        monthCellRender={dateMonthRender}
-      />
+      <Row gutter={rowGutterSettings}>
+        <Col xs={24} sm={24} md={11} lg={9} xl={7} xxl={5}>
+          <div className="calendar__date-picker">
+            <AntDCalendar
+              validRange={[getBeginDate(dates), getEndDate(dates)]}
+              fullscreen={false}
+              value={value}
+              onSelect={onSelect}
+              onPanelChange={onPanelChange}
+              monthCellRender={dateMonthRender}
+            />
+          </div>
+        </Col>
+        <Col xs={24} sm={24} md={13} lg={15} xl={17} xxl={19}>
+          <Row gutter={rowGutterSettings}>
+            <Col xs={24} sm={24} md={24} lg={24} xl={24} xxl={24}>
+              <Row justify="end">
+                <Col>
+                  <Select defaultValue="1" style={{ width: '100%' }}>
+                    <Select.Option value="1">Day</Select.Option>
+                    <Select.Option value="3">3 Days</Select.Option>
+                    <Select.Option value="7">Week</Select.Option>
+                  </Select>
+                </Col>
+                <Col xs={24} sm={24} md={24} lg={24} xl={24} xxl={24}>
+                  <Divider>
+                    {selectedValue && selectedValue.format('MMMM Do YYYY')}
+                  </Divider>
+                </Col>
+              </Row>
+            </Col>
+            <Col xs={24} sm={24} md={24} lg={24} xl={24} xxl={24}>
+              <CalendarDate data={eventsSortByDate(getDayData(value))} date={value} />
+            </Col>
+          </Row>
+        </Col>
+      </Row>
     </div>
   );
 };
