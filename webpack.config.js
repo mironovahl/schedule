ac0da7fbf3775ad28f95c029fa6c1f3eb92f6b15
Oleg Rashinskiy <32981111+Kefan1997@users.noreply.github.com>
2020-09-08 21:25:14 +0200

    Rs-15 test(CI & Commit validation): add CI test clean:dist & commit-validation (#28)

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,6 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
-const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 
 module.exports = {
   entry: ['./src/index.ts'],
@@ -21,19 +20,21 @@ module.exports = {
           emitWarning: true,
           failOnError: false,
         },
-      }, {
+      },
+      {
         test: /\.(ts|jsx|tsx)$/,
         exclude: /node_modules/,
         use: ['babel-loader', 'ts-loader'],
-      }, {
+      },
+      {
         test: /\.(css|scss)$/,
-        use: [
-          MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader',
-        ],
-      }, {
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+      },
+      {
         test: /\.html$/,
         loader: 'html-loader',
-      }, {
+      },
+      {
         test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
         use: [
           {
