7809675aae68dd24e91880d74c36af53159d5ca0
Valeria <Valeryia.Karzhaneuskaya@mail.ru>
2020-09-10 21:40:43 +0300

    RS-23 feat(List-page): add link

--- src/components/pages/list-page.tsx
+++ src/components/pages/list-page.tsx
@@ -14,6 +14,7 @@ const ListPage: React.FC = () => {
   const backendService = new BackendService();
   const [listData, setListData] = useState<IEvent[]>();
   const [loading, setLoading] = useState<boolean>(true);
+  let selected: never[] = [];
 
   useEffect(() => {
     setLoading(true);
@@ -25,32 +26,49 @@ const ListPage: React.FC = () => {
       .catch(() => setLoading(false));
   }, []);
 
-  function onChecked(e: { target: { checked: any; }; }) {
-    // eslint-disable-next-line no-console
-    console.log(`checked = ${e.target.checked}`);
-  }
+  const onChange = (checkedValues: any) => {
+    selected = checkedValues;
+    localStorage.setItem('checked', JSON.stringify(selected));
+  };
 
   return (
     <PageLayout loading={loading} title="List">
-      <List
-        itemLayout="horizontal"
-        dataSource={listData}
-        pagination={{
-          pageSize: 6,
-        }}
-        renderItem={(item) => (
-          <List.Item
-            className="list-item"
-            key={item.id}
-            actions={[<a href={`/task-page/${item.id}`} key="list-item__load-more">more</a>]}
-          >
-            <Checkbox onChange={onChecked} style={{ margin: 10 }} />
-            <List.Item.Meta description={moment(item.date).format('DD-MM-YYYY')} />
-            <List.Item.Meta title={<a href={item.url}>{item.name}</a>} />
-            <RenderTag type={item.type} />
-          </List.Item>
-        )}
-      />
+      <Checkbox.Group onChange={onChange} style={{ width: '100%' }}>
+        <List
+          itemLayout="horizontal"
+          dataSource={listData}
+          pagination={{
+            pageSize: 6,
+          }}
+          renderItem={(item) => (
+            <List.Item
+              className="list-item"
+              key={item.id}
+              actions={[<a href={`/task-page/${item.id}`} key="list-item__load-more">more</a>]}
+            >
+              <Checkbox value={item.id} style={{ margin: 10 }} />
+              <List.Item.Meta
+                title={item.name}
+                description={<RenderTag type={item.type} />}
+              />
+              <List.Item.Meta description={(
+                <a
+                  href={item.url}
+                  rel="noreferrer"
+                  target="_blank"
+                >
+                  Ссылка
+                </a>
+            )}
+              />
+              <List.Item.Meta
+                title={moment(item.date).format('DD-MM-YYYY')}
+                description={moment(item.date).format('h:mm')}
+              />
+            </List.Item>
+          )}
+        />
+      </Checkbox.Group>
     </PageLayout>
   );
 };
