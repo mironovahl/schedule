a903c12da89b96b94ea5b4563c37b44fb64bdb96
Mergen <gaydaev.mergen@gmail.com>
2020-09-10 22:25:30 +0300

    RS-24 feat(settings): Implement User Settings (#35)

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,34 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   BrowserRouter as Router, Route, Switch,
 } from 'react-router-dom';
 
 import './app.scss';
+import SettingsContext from '../../context/settings-context';
+import SettingsService from '../../services/settings-service';
 import {
   MainPage, SchedulePage, Page404, TaskPage,
 } from '../pages';
+import { ISettings } from '../../interfaces/settings-interfaces';
 
-const Schedule: React.FC = () => (
-  <Router>
-    <Switch>
-      <Route
-        path="/"
-        component={MainPage}
-        exact
-      />
-      <Route
-        path="/schedule"
-        component={SchedulePage}
-      />
-      <Route
-        path="/task-page/:id"
-        component={TaskPage}
-      />
-      <Route
-        component={Page404}
-      />
-    </Switch>
-  </Router>
-);
+const Schedule: React.FC = () => {
+  const [settings, changeSettings] = useState<ISettings>(SettingsService.getAllSettings());
+  const changeContext = (newSettings: ISettings) => {
+    changeSettings(newSettings);
+  };
+
+  return (
+    <SettingsContext.Provider value={{ ...settings, changeContext }}>
+      <Router>
+        <Switch>
+          <Route
+            path="/"
+            component={MainPage}
+            exact
+          />
+          <Route
+            path="/schedule"
+            component={SchedulePage}
+          />
+          <Route
+            path="/task-page/:id"
+            component={TaskPage}
+          />
+          <Route
+            component={Page404}
+          />
+        </Switch>
+      </Router>
+    </SettingsContext.Provider>
+  );
+};
 
 export default Schedule;
---
+++ src/components/settings-bar/settings-bar.tsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import {
+  Row,
+  Col,
+  Select,
+} from 'antd';
+import { ScheduleView, Timezone, ITaskSettings } from '../../interfaces/settings-interfaces';
+import timezones from '../../config/timezones';
+import ColorSettings from './color-settings/color-settings';
+
+type SettingsBarProps = {
+  view: ScheduleView;
+  onViewChange: (value: ScheduleView) => void;
+  timezone: Timezone;
+  onTimezoneChange: (value: Timezone) => void;
+  tasksSettings: ITaskSettings;
+  onTasksSettingsChange: (value: ITaskSettings) => void;
+};
+
+const SettingsBar: React.FC<SettingsBarProps> = ({
+  view,
+  onViewChange,
+  timezone,
+  onTimezoneChange,
+  tasksSettings,
+  onTasksSettingsChange,
+}: SettingsBarProps) => (
+  <div className="settings-bar">
+    <Row align="middle" justify="space-between">
+      <Col>
+        <Select defaultValue={view} onChange={onViewChange}>
+          <Select.Option value="table" key={1}>Table</Select.Option>
+          <Select.Option value="calendar" key={2}>Calendar</Select.Option>
+          <Select.Option value="list" key={3}>List</Select.Option>
+        </Select>
+        <Select defaultValue={timezone} onChange={onTimezoneChange}>
+          {timezones.map((t) => (
+            <Select.Option value={t} key={t}>{t}</Select.Option>
+          ))}
+        </Select>
+      </Col>
+      <Col>
+        <ColorSettings defaultValue={tasksSettings} onChange={onTasksSettingsChange} />
+      </Col>
+    </Row>
+  </div>
+);
+
+export default SettingsBar;
