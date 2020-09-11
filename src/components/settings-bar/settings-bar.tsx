36542454216b86d6f49f4ec2231dd1613d1d0c9f
Oleg Rashinskiy <oleg.rashinskiy@gmail.com>
2020-09-11 22:01:43 +0300

    RS-38 feat(Menu): start side menu

--- src/components/settings-bar/settings-bar.tsx
+++ src/components/settings-bar/settings-bar.tsx
@@ -3,7 +3,9 @@ import {
   Row,
   Col,
   Select,
+  // Menu,
 } from 'antd';
+import { UnorderedListOutlined, TableOutlined, CalendarOutlined } from '@ant-design/icons';
 import { ScheduleView, Timezone, TaskSettings } from '../../interfaces/settings-interfaces';
 import timezones from '../../config/timezones';
 import ColorSettings from './color-settings/color-settings';
@@ -28,14 +30,25 @@ const SettingsBar: React.FC<SettingsBarProps> = ({
   <div className="settings-bar">
     <Row align="middle" justify="space-between">
       <Col>
+        <UnorderedListOutlined />
+        <TableOutlined />
+        <CalendarOutlined />
         <Select defaultValue={view} onChange={onViewChange}>
-          <Select.Option value="table" key={1}>Table</Select.Option>
-          <Select.Option value="calendar" key={2}>Calendar</Select.Option>
-          <Select.Option value="list" key={3}>List</Select.Option>
+          <Select.Option value="table" key={1}>
+            Table
+          </Select.Option>
+          <Select.Option value="calendar" key={2}>
+            Calendar
+          </Select.Option>
+          <Select.Option value="list" key={3}>
+            List
+          </Select.Option>
         </Select>
         <Select defaultValue={timezone} onChange={onTimezoneChange}>
           {timezones.map((t) => (
-            <Select.Option value={t} key={t}>{t}</Select.Option>
+            <Select.Option value={t} key={t}>
+              {t}
+            </Select.Option>
           ))}
         </Select>
       </Col>
