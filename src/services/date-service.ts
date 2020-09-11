cbc5d46dcb7b78ae4d2c4e6f2edef5c7d5fe12bf
RemyJS <a.king92@mail.ru>
2020-09-11 22:58:30 +0300

    RS-25 feat(date-service): implement date service

---
+++ src/services/date-service.ts
@@ -0,0 +1,33 @@
+import { useContext } from 'react';
+import * as moment from 'moment-timezone';
+import SettingsContext from '../context/settings-context';
+import { IEvent } from '../interfaces/backend-interfaces';
+
+const getFullDate = (date: moment.Moment) => {
+  const { timezone } = useContext(SettingsContext);
+  return date.tz(timezone).format('MMMM Do YYYY');
+};
+
+const getDate = (date: moment.Moment) => {
+  const { timezone } = useContext(SettingsContext);
+  return date.tz(timezone).format('DD-MM-YYYY');
+};
+
+const getTime = (date: moment.Moment) => {
+  const { timezone } = useContext(SettingsContext);
+  return date.tz(timezone).format('H:mm');
+};
+
+const eventsSortByDate = (events: IEvent[]): IEvent[] => events
+  .sort((a: IEvent, b: IEvent): number => {
+    const dateA = moment(a.date);
+    const dateB = moment(b.date);
+
+    if (dateA.isBefore(dateB)) return -1;
+    if (dateA.isSame(dateB)) return 0;
+    return 1;
+  });
+
+export {
+  getFullDate, getDate, getTime, eventsSortByDate,
+};
