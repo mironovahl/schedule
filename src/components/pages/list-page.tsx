38add665e3ba2beee6f5c1e26a91703d550c806b
Valeria <Valeryia.Karzhaneuskaya@mail.ru>
2020-09-14 22:17:32 +0300

    RS-23 feat(ListPage): add list to main page

--- src/components/pages/list-page.tsx
+++ src/components/pages/list-page.tsx
@@ -1,76 +1,83 @@
-import React, { useState, useEffect } from 'react';
+import React, { useContext } from 'react';
 
-import { List, Checkbox } from 'antd';
+import { List, Checkbox, Button } from 'antd';
 
 import moment from 'moment';
 import RenderTag from '../type-task';
-import BackendService from '../../services/backend-service';
-import PageLayout from '../page-layout';
 import { IEvent } from '../../interfaces/backend-interfaces';
-
+import SettingsContext from '../../context/settings-context';
 import './list-page.scss';
 
-const ListPage: React.FC = () => {
-  const backendService = new BackendService();
-  const [listData, setListData] = useState<IEvent[]>();
-  const [loading, setLoading] = useState<boolean>(true);
-  let selected: never[] = [];
+type ListProps = {
+  dataSource: IEvent[] | undefined;
+};
 
-  useEffect(() => {
-    setLoading(true);
-    backendService.getAllEvents()
-      .then((data) => {
-        setLoading(false);
-        setListData([...data]);
-      })
-      .catch(() => setLoading(false));
-  }, []);
+const ListPage: React.FC<ListProps> = ({ dataSource }: ListProps) => {
+  const { changeContext } = useContext(SettingsContext);
+  const selected: any[] = [];
 
-  const onChange = (checkedValues: any) => {
-    selected = checkedValues;
-    localStorage.setItem('checked', JSON.stringify(selected));
+  const onChange = (e: { target: any; }) => {
+    if (e.target.checked) {
+      selected.push(e.target.id);
+    } else {
+      selected.splice(selected.indexOf(e.target.id), 1);
+    }
+  };
+
+  const hideElement = () => {
+    const hidden = dataSource?.filter((i) => selected.includes(i.id))
+    .concat(selected.filter((i: IEvent) => dataSource.includes(i)));
+    changeContext({ hiddenRows: hidden });
   };
 
   return (
-    <PageLayout loading={loading} title="List">
-      <Checkbox.Group onChange={onChange} style={{ width: '100%' }}>
-        <List
-          itemLayout="horizontal"
-          dataSource={listData}
-          pagination={{
-            pageSize: 6,
-          }}
-          renderItem={(item) => (
-            <List.Item
-              id={item.id}
-              className="list-item"
-              key={item.id}
-              actions={[<a href={`/task-page/${item.id}`} key="list-item__load-more">See more</a>]}
-            >
-              <Checkbox value={item.id} style={{ margin: 10 }} />
-              <List.Item.Meta
-                title={item.name}
-                description={<RenderTag type={item.type} />}
-              />
-              <List.Item.Meta description={(
-                <a
-                  href={item.url}
-                  rel="noreferrer"
-                  target="_blank"
-                >
-                  Ссылка
-                </a>
+    <List
+      header={(
+        <>
+          <Button type="primary" onClick={hideElement}>Hide</Button>
+          <Button type="primary" ghost>Hidden Rows</Button>
+        </>
+      )}
+      pagination={{
+        pageSize: 5,
+      }}
+      itemLayout="horizontal"
+      dataSource={dataSource}
+      renderItem={(item) => (
+        <List.Item
+          id={item.id}
+          className="list-item"
+          key={item.id}
+          actions={[<a href={`/task-page/${item.id}`} key="list-item__load-more">See more</a>]}
+        >
+          <Checkbox
+            onChange={onChange}
+            id={item.id}
+            style={{ margin: 10 }}
+          />
+          <List.Item.Meta
+            title={item.name}
+            description={<RenderTag type={item.type} />}
+          />
+          <List.Item.Meta
+            description={(
+              <a
+                href={item.url}
+                rel="noreferrer"
+                target="_blank"
+              >
+                Link
+              </a>
             )}
-              />
-              <List.Item.Meta
-                title={moment(item.date).format('DD-MM-YYYY')}
-                description={moment(item.date).format('h:mm')}
-              />
-            </List.Item>
-          )}
-        />
-      </Checkbox.Group>
-    </PageLayout>
+            className="list-item__description"
+          />
+          <List.Item.Meta
+            title={moment(item.date).format('DD-MM-YYYY')}
+            description={moment(item.date).format('h:mm')}
+          />
+        </List.Item>
+      )}
+    />
   );
 };
 
