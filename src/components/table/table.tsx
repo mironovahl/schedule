10a89b571fc1f3eb09bf18da4b123aee723d681d
Andrey <45938215+RemyJS@users.noreply.github.com>
2020-09-24 20:36:01 +0300

    Rs 90 add organizer to the table (#94)

--- src/components/table/table.tsx
+++ src/components/table/table.tsx
@@ -41,12 +41,13 @@ import {
   IColumnsVisibility,
   EditableCellProps,
 } from '../../interfaces/table-interfaces';
-import { IEvent } from '../../interfaces/backend-interfaces';
+import { IEvent, IOrganizer } from '../../interfaces/backend-interfaces';
 
 import './table.scss';
 
 type TableProps = {
   dataSource: IEvent[] | undefined;
+  organizers: IOrganizer[] | undefined;
 };
 
 const EditableCell: React.FC<EditableCellProps> = ({
@@ -99,8 +100,9 @@ function isDuplicate(arr: { text: string; value: string }[], value: string): boo
 }
 
 // eslint-disable-next-line sonarjs/cognitive-complexity
-const Table: React.FC<TableProps> = ({ dataSource }: TableProps) => {
+const Table: React.FC<TableProps> = ({ dataSource, organizers }: TableProps) => {
   if (!dataSource) return <Empty image={Empty.PRESENTED_IMAGE_SIMPLE} />;
+  if (!organizers) return <Empty image={Empty.PRESENTED_IMAGE_SIMPLE} />;
   const backendService = new BackendService();
   const [columnsVisible, setColumnsVisible] = useState<IColumnsVisibility>({
     done: true,
@@ -256,6 +258,20 @@ const Table: React.FC<TableProps> = ({ dataSource }: TableProps) => {
         </Tooltip>
       ),
       editable: true,
+    },
+    {
+      title: 'Organizer',
+      width: 100,
+      dataIndex: 'organizerID',
+      key: 'organizerID',
+      columnVisible: columnsVisible.description,
+      render: (organizerID: string) => (
+        <>
+          {
+            organizers.find((item) => item.id === organizerID)!.name
+          }
+        </>
+      ),
     },
     {
       title: 'Details Url',
